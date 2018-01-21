8c5aa5472e69c8c44d20aa3d5415b02c0e108023
kanwong <kanwong@gmail.com>
2018-01-21 13:34:31 +0800

    adding more languages

--- i18n/languages.js
+++ i18n/languages.js
@@ -9,5 +9,30 @@ const languages = {
     common: require('./locales/de/common.json'),
     flag: require('./flags/de.png'),
   },
+  ja: {
+    name: 'Japanese',
+    common: require('./locales/ja/common.json'),
+    flag: require('./flags/ja.png'),
+  },
+  cn: {
+    name: 'Chinese',
+    common: require('./locales/cn/common.json'),
+    flag: require('./flags/cn.png'),
+  },
+  ko: {
+    name: 'Korean',
+    common: require('./locales/ko/common.json'),
+    flag: require('./flags/ko.png'),
+  },
+  es: {
+    name: 'Spanish',
+    common: require('./locales/es/common.json'),
+    flag: require('./flags/es.png'),
+  },
+  ru: {
+    name: 'Russian',
+    common: require('./locales/ru/common.json'),
+    flag: require('./flags/ru.png'),
+  },
 };
 export default languages;
