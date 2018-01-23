ecebf814fb8b09c12fae28f7a72e6fdf8e40f6b5
kanwong <kanwong@gmail.com>
2018-01-23 22:38:08 +0800

    disable some of the languages which pending for prove read

--- i18n/languages.js
+++ i18n/languages.js
@@ -9,35 +9,5 @@ const languages = {
     common: require('./locales/de/common.json'),
     flag: require('./flags/de.png'),
   },
-  ja: {
-    name: 'Japanese',
-    common: require('./locales/ja/common.json'),
-    flag: require('./flags/ja.png'),
-  },
-  cn: {
-    name: 'Chinese',
-    common: require('./locales/cn/common.json'),
-    flag: require('./flags/cn.png'),
-  },
-  ko: {
-    name: 'Korean',
-    common: require('./locales/ko/common.json'),
-    flag: require('./flags/ko.png'),
-  },
-  es: {
-    name: 'Spanish',
-    common: require('./locales/es/common.json'),
-    flag: require('./flags/es.png'),
-  },
-  pt: {
-    name: 'Portuguese',
-    common: require('./locales/pt/common.json'),
-    flag: require('./flags/pt.png'),
-  },
-  ru: {
-    name: 'Russian',
-    common: require('./locales/ru/common.json'),
-    flag: require('./flags/ru.png'),
-  },
 };
 export default languages;
