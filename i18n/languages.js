f29213264610254dbbc72a8ac88895a1a7787814
kanwong <kanwong@gmail.com>
2018-01-25 22:14:44 +0800

    closed #1

--- i18n/languages.js
+++ i18n/languages.js
@@ -9,5 +9,10 @@ const languages = {
     common: require('./locales/de/common.json'),
     flag: require('./flags/de.png'),
   },
+  ja: {
+    name: 'Japanese',
+    common: require('./locales/ja/common.json'),
+    flag: require('./flags/ja.png'),
+  },
 };
 export default languages;
